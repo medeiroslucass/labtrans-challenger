6b4c012c0b56fa7b5d547be63b90202fb3cee8e0
medeiroslucass <lcsmedeiroscosta@gmail.com>
2023-07-04 22:08:01 -0300

    feat: Adding Database Connection and Mapping the Results Table to the Database
    Signed-off-by: medeiroslucass <lcsmedeiroscosta@gmail.com>

---
+++ models.py
@@ -0,0 +1,28 @@
+from peewee import *
+
+db = SqliteDatabase('labtrans.db')
+
+class BaseModel(Model):
+
+    class Meta:
+        database = db
+
+
+class Results(BaseModel):
+    name = CharField()
+    km = DoubleField()
+    distance = DoubleField()
+    highway = IntegerField()
+    item = CharField()
+
+def create_database():
+    db.connect()
+    db.create_tables(
+        [
+            Results
+        ]
+    )
+
+    db.close()
+
+create_database()
