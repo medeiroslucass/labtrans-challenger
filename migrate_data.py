2ff8a62a6e3ed85dcce1d38c68323cde19123a18
medeiroslucass <lcsmedeiroscosta@gmail.com>
2023-07-10 17:34:11 -0300

    fix: fixiing migrate_data script
    Signed-off-by: medeiroslucass <lcsmedeiroscosta@gmail.com>

--- migrate_data.py
+++ migrate_data.py
@@ -2,10 +2,43 @@ import os
 import pandas as pd
 import csv
 from models import Results, Video, Rodovia
+from peewee import *
 
 # Pasta onde estão os arquivos CSV
 pasta_csv_video = './videos'
-pasta_csv_rodovia = './csv_files'
+
+
+def create_csv_highway_files():
+    highways = Results.select(Results.highway).distinct()
+
+    # Cria a pasta "csv_files" se não existir
+    folder_path = "csv_files"
+    os.makedirs(folder_path, exist_ok=True)
+
+    for highway in highways:
+        query = Results.select(
+            Results.highway,
+            Results.km,
+            fn.SUM(Results.item == "Buraco").alias("buraco"),
+            fn.SUM(Results.item == "Remendo").alias("remendo"),
+            fn.SUM(Results.item == "Trinca").alias("trinca"),
+            fn.SUM(Results.item == "Placa").alias("placa"),
+            fn.SUM(Results.item == "Drenagem").alias("drenagem")
+        ).where(Results.highway == highway.highway).group_by(Results.highway, Results.km)
+
+        results = query.dicts()
+
+        if results:
+            filename = f"resultados_{highway.highway}.csv"
+            file_path = os.path.join(folder_path, filename)
+
+            with open(file_path, 'w', newline='') as file:
+                fieldnames = ['highway', 'km', 'buraco', 'remendo', 'trinca', 'placa', 'drenagem']
+                writer = csv.DictWriter(file, fieldnames=fieldnames)
+                writer.writeheader()
+                writer.writerows(results)
+
+    print("CSV files exported successfully.")
 
 
 # Função para popular a tabela Video
@@ -46,30 +79,23 @@ def insert_videos_table():
 
 # Função para popular a tabela Rodovia
 def insert_rodovia_table():
+    pasta_csv_rodovia = "csv_files"  # Pasta onde os arquivos CSV foram criados
+
     # Percorrer os arquivos CSV na pasta
     for arquivo in os.listdir(pasta_csv_rodovia):
         if arquivo.endswith('.csv'):
             # Obter o número da rodovia do nome do arquivo
             rodovia_numero = int(arquivo.split('_')[1].split('.')[0])
-            print(f"RODOVIA: {rodovia_numero}")
             # Abrir o arquivo CSV
             caminho_arquivo = os.path.join(pasta_csv_rodovia, arquivo)
             with open(caminho_arquivo, 'r', newline='') as file:
                 reader = csv.DictReader(file)
+                rows = list(reader)
 
                 # Obter os valores de km_ini e km_final a partir dos dados do arquivo CSV
-                km_ini = None
-                km_final = None
-                for row in reader:
-                    if km_ini is None or km_final is None:
-                        km_ini = float(row['km'])
-                        km_final = float(row['km'])
-                    else:
-                        km = float(row['km'])
-                        if km < km_ini:
-                            km_ini = km
-                        if km > km_final:
-                            km_final = km
+                km_values = [float(row['km']) for row in rows]
+                km_ini = min(km_values)
+                km_final = max(km_values)
 
                 # Criar o registro na tabela Rodovia
                 rodovia = Rodovia.create(
@@ -112,8 +138,11 @@ def insert_result_table():
     # Exibe o número total de registros inseridos
     print(f"Total de registros inseridos: {total_registros}")
 
-
+insert_result_table()
+# Chamada para criar os csv das rodovias
+create_csv_highway_files()
+insert_rodovia_table()
 # Chamada das funções para popular as tabelas
 insert_videos_table()
-insert_rodovia_table()
-insert_result_table()
+
+
